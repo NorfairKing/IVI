ef573ba3f9057d5d5d9ccc8a8e89f22edf82cc79
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-07-08 06:17:24 +0200

    Some more comments

--- src/Ivi.hs
+++ src/Ivi.hs
@@ -8,19 +8,26 @@ import System.Exit (exitWith)
 main :: IO ()
 main = do
     args <- getArgs
+    
+    -- Try to make out which script is meant.
     case recognise args of
+    
+        -- Run the arguments as a shell command if no script is recognised.
         Nothing -> do
             phandle <- runCommand $ unwords args
             exitcode <- waitForProcess phandle
             exitWith exitcode
+            
+        -- Run the script that is recognised.
         Just scriptArgs -> putStrLn "placeholder"
 
 
-
+-- The datastructure that is given to run a script.
 data IVIScriptArgs = ScriptArgs
-                        [String] -- Raw command
-                         String  -- Script name
+                        String -- Raw command
+                        String  -- Script name
 
 
+-- Try to make out which script is meant by the given arguments.
 recognise :: [String] -> Maybe IVIScriptArgs
 recognise args = Nothing -- ScriptArgs args scriptName
