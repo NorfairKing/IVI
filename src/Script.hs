5320f6258cc5a573d163f398efb510163cbd4c2c
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-07-09 10:43:44 +0200

    finally started prettying

--- src/Script.hs
+++ src/Script.hs
@@ -5,9 +5,10 @@ module Script where
 import Language.Haskell.Interpreter
 import Data.Typeable.Internal
 
+import Constants
+
 -- The datastructure that is given to run a script.
-data IVIScriptArgs = Placeholder 
-                     | Args
+data IVIScriptArgs = Args
                         String -- Raw command
                         String -- Script name
                      deriving (Show ,Typeable)
@@ -17,37 +18,37 @@ data IVIScript = Script
                     FilePath -- Script dir
                      deriving (Show)
 
+-- TODO remove
+testExecute = executeScript (Script "TestScript" "/home/syd/ivi/scripts/TestScript.hs") (Args "heloo" "sweety")
 
-executeDynamic :: IVIScript -> IVIScriptArgs -> Interpreter (IO ())
-executeDynamic (Script mod path) args=
+executeDynamic :: IVIScript -> IVIScriptArgs -> Interpreter ()
+executeDynamic (Script modu path) args=
     do
       -- Load the script module
       loadModules [path]
 
       -- Set the imports that the script can use
-      setImportsQ [("Prelude", Nothing), ("Script", Nothing), (mod, Just qualification)]
+      setImportsQ [
+                     ("Prelude", Nothing)
+                   , ("Script", Nothing)
+                   , (modu, Just qualification) -- The script itself
+                  ]
       
       -- Define the name of the function to be gotten
-      let action = qualification ++ "." ++ fun
+      let action = qualification ++ "." ++ iviScriptFunction
        
       -- Get the function 
-      toEx <- interpret action (as :: IVIScriptArgs -> IO ())
+      ioFunction <- interpret action (as :: IVIScriptArgs -> IO ())
       
       -- evaluate it
-      return $ toEx args
+      liftIO $ ioFunction args
           where 
-            qualification = "DynModule"
-            fun = "execute"
+            qualification = "DynModule" -- Just to avoid conflicting names
+      
 
-
-testExecute :: IO()
-testExecute = do
-  let testScript = Script "TestScript" "/home/syd/ivi/scripts/TestScript.hs"
-  r <- runInterpreter $ executeDynamic testScript (Args "wut" "wut²")
+executeScript :: IVIScript -> IVIScriptArgs -> IO()
+executeScript script args = do
+  r <- runInterpreter $ executeDynamic script args
   case r of
       Left e -> putStrLn $ "woops... " ++ show e
-      Right stuff -> stuff
-
-
-say :: String -> Interpreter ()
-say = liftIO . putStrLn
+      Right _ -> return ()
