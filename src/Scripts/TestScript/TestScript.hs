08e14fd46fd40c8e8d6a9cb5f7ef7e85c8e9f2ce
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-07-25 20:01:08 +0200

    removed test scripts

--- src/Scripts/TestScript/TestScript.hs
+++
@@ -1,9 +0,0 @@
-module Scripts.TestScript.TestScript where
-
-import Script
-
-execute :: IVIScriptArgs -> IO IVIScriptResult
-execute _ = 
-    do 
-        putStrLn "this is testscript 1"
-        return Success
--- src/Scripts/TestScript2/TestScript.hs
+++
@@ -1,9 +0,0 @@
-module Scripts.TestScript2.TestScript where
-
-import Script
-
-execute :: IVIScriptArgs -> IO IVIScriptResult
-execute _ = 
-    do 
-        putStrLn "this is test script 2"
-        return Success
