372593031f38ac29f194c6111ee9f83f3b68915b
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-09-08 18:16:50 +0200

    more on dependencies

--- src/Dependency.hs
+++ src/Dependency.hs
@@ -11,9 +11,10 @@ checkDependency dep = do
     nullHandleOut <- openFile "/dev/null" AppendMode
     nullHandleIn  <- openFile "/dev/null" ReadMode
     let process = (shell cmd)
-            { std_out  = UseHandle nullHandleOut
+            { std_out = UseHandle nullHandleOut
             , std_err = UseHandle nullHandleOut
-            , std_in  = UseHandle nullHandleIn}
+            , std_in  = UseHandle nullHandleIn }
+
     (_, _, _, ph) <- createProcess process 
     ex <- waitForProcess ph
     case ex of
@@ -23,10 +24,14 @@ checkDependency dep = do
     where
         cmd = "command -v" ++ " " ++ dep
 
-withDependency :: Dependency -> (IO ()) -> IO ()
+withDependency :: Dependency -> IO a -> IO a
 withDependency dep fun = do
     meets <- checkDependency dep
     if meets
     then fun
     else error $ "Missing dependency: " ++ dep
+
+withDependencies :: [Dependency] -> IO a -> IO a
+withDependencies [] fun = fun
+withDependencies (d:ds) fun = withDependency d $ withDependencies ds fun
 
