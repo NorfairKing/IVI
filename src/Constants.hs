ef48f8425de3648f95075ddf36de95690df87966
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-09-09 08:17:33 +0200

    added some more constants

--- src/Constants.hs
+++ src/Constants.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 {-|
 Module      : Constants
 Description : IVI's constant values
@@ -6,15 +8,19 @@ module Constants (
     -- * Strings
       iviName
     , iviExtension
-    , iviDirname
+    , iviDirName
     -- * Paths
-    , iviDirectory
+    , iviHomeDirectory
+    , iviSrcDirectory
     ) where
 
-import System.Directory (getHomeDirectory)
-import System.FilePath.Posix ((</>))
+import System.Directory (getHomeDirectory, getCurrentDirectory)
+import System.FilePath ((</>), takeDirectory)
+--import System.FilePath.Posix ((</>))
+import Data.Functor ((<$>))
+import Language.Haskell.TH (runIO, location, stringL, litE, loc_filename)
 
-iviName, iviExtension, iviDirname :: String
+iviName, iviExtension, iviDirName :: String
 
 -- | The name of the program. This is constant, yes.
 iviName = "IVI"
@@ -23,9 +29,20 @@ iviName = "IVI"
 iviExtension = "ivi"
 
 -- | The name of the ivi config directory
-iviDirname = "." ++ iviName
+iviDirName = "." ++ iviName
 
 
--- | The users IVI directory 
-iviDirectory :: IO FilePath
-iviDirectory = getHomeDirectory >>= (\x -> return $ x </> iviDirname)
+-- | The users IVI directory in the home directory: ~/.IVI
+iviHomeDirectory :: IO FilePath
+iviHomeDirectory = getHomeDirectory >>= (\x -> return $ x </> iviDirName)
+
+-- | The absolute path to the parent of the source directory this file is in.
+iviRootDirectory :: FilePath
+iviRootDirectory = takeDirectory iviSrcDirectory
+
+-- | The absolute path to the source directory of ivi code
+iviSrcDirectory :: FilePath
+iviSrcDirectory = $(do 
+    dir <- runIO getCurrentDirectory
+    filename <- loc_filename <$> location
+    litE $ stringL $ dir </> filename)
