697caf8a1a0052553105df65dfa2b677ec3f1662
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-09-09 08:25:33 +0200

    simplified dependency immensely

--- src/Dependency.hs
+++ src/Dependency.hs
@@ -1,28 +1,15 @@
 module Dependency where
 
-import System.Process
-import System.IO
-import System.Exit
+import System.Directory (findExecutable)
 
 type Dependency = String -- Name
 
 checkDependency :: Dependency -> IO Bool
 checkDependency dep = do
-    nullHandleOut <- openFile "/dev/null" AppendMode
-    nullHandleIn  <- openFile "/dev/null" ReadMode
-    let process = (shell cmd)
-            { std_out = UseHandle nullHandleOut
-            , std_err = UseHandle nullHandleOut
-            , std_in  = UseHandle nullHandleIn }
-
-    (_, _, _, ph) <- createProcess process 
-    ex <- waitForProcess ph
-    case ex of
-        ExitSuccess -> return True
-        ExitFailure _ -> return False
-    
-    where
-        cmd = "command -v" ++ " " ++ dep
+    mf <- findExecutable dep
+    case mf of
+        Just fp -> return True
+        Nothing -> return False
 
 withDependency :: Dependency -> IO a -> IO a
 withDependency dep fun = do
