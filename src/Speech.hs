5fa119d80bd3a7062168ff6c157d7c7c880704e3
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-09-09 11:29:03 +0200

    started on native speech, looks like its working

---
+++ src/Speech.hs
@@ -0,0 +1,25 @@
+module Speech where
+
+import Dependency (withDependencies)
+import System.Process (runCommand, waitForProcess)
+
+say :: String -> IO ()
+say str = withDependencies [playBin, generationBin] go
+    where
+        go = do
+            generateSound
+            playSound
+        generateSound = do
+                let cmd = unwords [generationBin, "-l=" ++ language, "--wave=" ++ soundFile, "\"" ++ str ++ "\""]
+                h <- runCommand $ cmd
+                _ <- waitForProcess h
+                return ()
+        playSound = do
+                let cmd = unwords [playBin, "--no-show-progress", soundFile ]
+                h <- runCommand cmd
+                _ <- waitForProcess h
+                return ()
+        soundFile = "/tmp/speech.wav"
+        generationBin = "pico2wave"
+        playBin = "play"
+        language = "en-GB"
